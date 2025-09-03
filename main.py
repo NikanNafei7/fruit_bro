6a0a2cefe90432e32ff00b487e06176fa262664d
NikanNafei7 <Nikan.Nafei.7@gmail.com>
2025-09-03 15:47:28 +0330

    background

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import pygame
 import OpenGL.GL as gl
 import imgui
 from imgui.integrations.pygame import PygameRenderer
+import sys
+import os
+import random
 
-size = 800, 600
+size = 1200, 800
 pygame.display.set_mode(size, pygame.OPENGL | pygame.DOUBLEBUF)
+pygame.display.set_caption("fruit bro")
 
 imgui.create_context()
 
@@ -14,6 +18,20 @@ io.display_size = size
 renderer = PygameRenderer()
 
 clock = pygame.time.Clock()
+
+r_1to4 = random.randint(1, 4)
+def background(number):
+    backgrounds = {
+        1:(0/255.0, 51/255.0, 34/255.0, 1), #dark green
+        2:(123/255.0, 129/255.0, 32/255.0, 1.0), #yellow
+        3:(51/255.0, 0/255.0, 102/255.0, 1.0), #purple
+        4:(0/255.0, 0/255.0, 102/255.0, 1.0), #dark blue
+    }
+    gl.glClearColor(*backgrounds[number])
+
+
+def main():
+    pass
 
 running = True
 while running:
@@ -31,7 +49,7 @@ while running:
 
     imgui.end()
 
-    gl.glClearColor(0.145, 0.34, 0.123, 1)
+    background(r_1to4)
     gl.glClear(gl.GL_COLOR_BUFFER_BIT)
     imgui.render()
     renderer.render(imgui.get_draw_data())
