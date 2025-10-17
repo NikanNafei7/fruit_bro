d716ac0d3083194a7bef3c3461f6a262d867a757
NikanNafei7 <Nikan.Nafei.7@gmail.com>
2025-10-17 16:41:20 +0330

    added icon and module

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 import pygame
 import pygame_gui
-from button import Button
+from modules.button import Button
 import sys
 import os
 import random
+
+BASE_DIR = os.path.dirname(__file__)
 
 pygame.init()
 
 size = 1200, 800
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("fruit bro")
+icon_path = os.path.join(BASE_DIR, "assets", "icon.png")
+icon = pygame.image.load(icon_path)
+pygame.display.set_icon(icon)
 
 clock = pygame.time.Clock()
 
 running = True
 
-BASE_DIR = os.path.dirname(__file__)
-
 def get_font(size):
     return pygame.font.Font("assets/font.ttf", size)
 
-rand_1to4 = random.randint(1, 4)
+rand_1to4_b = random.randint(1, 4)
 def background(number):
     backgrounds = {
         1:(0, 51, 34), #dark green
@@ -30,21 +33,24 @@ def background(number):
     }
     screen.fill(backgrounds.get(number))
 
-rand_1to3 = random.randint(1, 3)
+rand_1to4_r = random.randint(1, 4)
 rocks_list = []
 def rock(number):
     rock1_path = os.path.join(BASE_DIR, "assets", "rock1.png") #X=101 , Y=64
     rock2_path = os.path.join(BASE_DIR, "assets", "rock2.png") #X=101 , Y=64
     rock3_path = os.path.join(BASE_DIR, "assets", "rock3.png") #X=101 , Y=64
+    rock4_path = os.path.join(BASE_DIR, "assets", "rock4.png") #X=101 , Y=64
     rock1_img = pygame.image.load(rock1_path).convert_alpha()
     rock2_img = pygame.image.load(rock2_path).convert_alpha()
     rock3_img = pygame.image.load(rock3_path).convert_alpha()
+    rock4_img = pygame.image.load(rock4_path).convert_alpha()
     rocks = {
         1:rock1_img,
         2:rock2_img,
         3:rock3_img,
+        4:rock4_img,
     }
-    for i in range(37):
+    for i in range(40):
         rock_x = random.randint(0, 1099)
         rock_y = random.randint(0, 736)
         rocks_list.append((rocks.get(number), (rock_x, rock_y)))
@@ -60,7 +66,7 @@ class Enemy:
         self.speed = speed
 
 def play():
-    rock(rand_1to3)
+    rock(rand_1to4_r)
 
     global running
     while running:
@@ -68,9 +74,9 @@ def play():
             if event.type == pygame.QUIT:
                 running = False
 
-        background(rand_1to4)
+        background(rand_1to4_b)
         show_rock()
-        pygame.display.flip()
+        pygame.display.update()
         clock.tick(60)
 
 mmbg_path = os.path.join(BASE_DIR, "assets", "mm_Background.png")
@@ -82,8 +88,8 @@ def main_menu():
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+        MENU_TEXT = get_font(100).render("FRUIT\n BRO", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 140))
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 400), 
                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
@@ -98,20 +104,18 @@ def main_menu():
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+                running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     play()
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     running = False
 
-        pygame.display.flip()
+        pygame.display.update()
 
 
 def main():
     main_menu()
-    play()
 
     pygame.quit()
 
